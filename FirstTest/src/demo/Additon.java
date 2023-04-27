cdf3ef554073d4a5f961f404b2059ac582fe3a6e
sawanlilhare2023 <sawanlilhare7@gmail.com>
2023-04-27 13:30:10 +0530

    four integer addditon

--- FirstTest/src/demo/Additon.java
+++ FirstTest/src/demo/Additon.java
@@ -5,8 +5,8 @@ public class Additon {
 	public static void main(String[] args) {
 		int a= 10;
 		int b= 20;
-		int c=10;
-		int d= 20;
+		 int c=10;
+		 int d= 20;
 		System.out.println(a+b+c);
 		
 
